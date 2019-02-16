33560e7744fe6671784ba2027cf4705e85fa1b04
Franco Ivan Zuccarellli <francoz@axoft.com>
2019-02-16 18:17:39 -0300

    Recording video and audio - Initial example.

---
+++ Source/Modules/VideoReviews.DAL/Domain/Entities/VideoReview.cs
@@ -0,0 +1,20 @@
+﻿using RepositoryDatabaseAccess.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoReviews.DAL.Domain.Entities
+{
+    public class VideoReview : AuditoryEntity
+    {
+        public long VideoID { get; set; }
+
+        [ForeignKey("VideoID")]
+        public Video Video { get; set; }
+
+        public string Url { get; set; }
+    }
+}
---
+++ Source/VideoReviews/Controllers/VideoRecordingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoReviews.Controllers
+{
+    public class VideoRecordingController : Controller
+    {
+        // GET: Video
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult UploadReview(long videoId, HttpPostedFileBase file)
+        {
+            var videoName = $"{Guid.NewGuid().ToString()}.webm";
+            string nameAndLocation = $"~/Reviews/video{videoId}/{videoName}";
+            file.SaveAs(Server.MapPath(nameAndLocation));
+            return Json("ok");
+        }
+    }
+}
